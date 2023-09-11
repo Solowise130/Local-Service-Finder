81f8d8a445322582f3921d82c03cb4a299487f54
mazi-kunle <olakunlemichael644@gmail.com>
2023-09-11 13:24:29 +0100

    create service model

---
+++ backend/models/service.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''A module for service'''
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from models.user import Base
+
+
+class Service(Base):
+    '''A service class
+    '''
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    service_providers = relationship('Service_provider') # one to many relatioship
