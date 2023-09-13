b66c263e65581dcc49567df31a50b6adf8a07299
mazi-kunle <olakunlemichael644@gmail.com>
2023-09-13 12:54:02 +0100

    add function for adding service providers

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@
 '''DB module
 '''
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from models.user import Base
 from models.service import Service
 from models.review import Review
@@ -25,6 +25,33 @@ class DB:
         )
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
+        DBSession = sessionmaker(bind=self._engine)
+        self.__session = DBSession()
 
+    def add_service_provider(self, **kwargs):
+        '''Add service providers
+        '''
+        first_name = kwargs.get('firstName')
+        last_name = kwargs.get('lastname')
+        location = kwargs.get('location')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+        service_id = kwargs.get('service')
+        contact_num = kwargs.get('contact_num')
+        description = kwargs.get('description')
+        created_at = kwargs.get('created_at')
 
-test = DB()
+        if not all([first_name, last_name, location, email,
+                password, service_id, contact_num, description,
+                created_at]):
+            return None
+        
+        new_service_provider = ServiceProvider()
+        self.__session.add(new_service_provider)
+        self.__session.commit()
+
+        return new_service_provider
+            
+        
+
+# test = DB()
